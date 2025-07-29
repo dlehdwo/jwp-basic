fa6afa55667d0e8ad1d2ea731ea67aa82686f37c
LeeDongjae <leedongjae625@gmail.com>
2025-07-29 22:06:15 +0900

    try-catch AutoClose 인터페이스 적용

--- src/main/java/next/dao/JdbcTemplate.java
+++ src/main/java/next/dao/JdbcTemplate.java
@@ -10,58 +10,25 @@ import java.sql.SQLException;
 public class JdbcTemplate {
 
     public void executeUpdate(String sql, PreparedStatementSetter pss) {;
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+        try (Connection con = ConnectionManager.getConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);){
             pss.setParameters(pstmt);
-
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new DataAccessException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException e) {
-                throw new DataAccessException(e);
-            }
         }
     }
 
     public <T> T executeQuery(String sql, RowMapper<T> rm, PreparedStatementSetter pss) {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
-
+        try (Connection con = ConnectionManager.getConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);){
             pss.setParameters(pstmt);
-            rs = pstmt.executeQuery();
-            return rm.mapRow(rs);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                return rm.mapRow(rs); // 로직 실행 후 rs는 자동 close
+            }
         } catch (SQLException e) {
             throw new DataAccessException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException e) {
-                throw new DataAccessException(e);
-            }
         }
     }
 
